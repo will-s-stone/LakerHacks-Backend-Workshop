2e2b3f05c9b3c01ce68829c19124261711fcafd8
will-s-stone <will@williamstone.me>
2025-04-16 13:02:00 -0400

    Init

---
+++ src/main/java/edu/lakerhacks/LakerHacks_Backend_Workshop/LakerHacksBackendWorkshopApplication.java
@@ -0,0 +1,13 @@
+package edu.lakerhacks.LakerHacks_Backend_Workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LakerHacksBackendWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LakerHacksBackendWorkshopApplication.class, args);
+	}
+
+}
